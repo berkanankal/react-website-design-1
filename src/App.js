c9ed3d68150952dbaa843496029f835d5f985b92
berkanankal <berkan.ankal@outlook.com>
2022-05-15 18:21:56 +0300

    Styling navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-    </div>
+    </BrowserRouter>
   );
 };
 
