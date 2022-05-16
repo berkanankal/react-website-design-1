0eb843c2ec4ca436b5f46625b371bf0a96e19bf0
berkanankal <berkan.ankal@outlook.com>
2022-05-16 20:21:43 +0300

    Styling sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Sidebar />
       <Navbar />
     </BrowserRouter>
   );
