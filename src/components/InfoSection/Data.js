6c73fd8f9b29ffef1092ebe1a6238161bac69c6f
berkanankal <berkan.ankal@outlook.com>
2022-05-18 16:58:27 +0300

    Creating & styling services section

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,4 +1,6 @@
 import svg1 from "../../images/svg-1.svg";
+import svg4 from "../../images/svg-4.svg";
+import svg3 from "../../images/svg-3.svg";
 
 export const homeObjOne = {
   id: "about",
@@ -17,4 +19,39 @@ export const homeObjOne = {
   dark: true,
   primary: true,
   darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lighttextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Login to your account at any time",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions whitout getting charged any fees.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: svg4,
+  alt: "Piggybanck",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lighttextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions whitout getting charged any fees.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: svg3,
+  alt: "car",
+  primary: false,
+  darkText: true,
 };
