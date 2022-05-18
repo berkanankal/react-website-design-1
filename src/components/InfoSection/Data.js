7d57ba6253e9d7e50b85997a7ace73e7dad55fd7
berkanankal <berkan.ankal@outlook.com>
2022-05-18 16:43:54 +0300

    Adding data to info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,20 @@
+import svg1 from "../../images/svg-1.svg";
+
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Lorem, ipsum.",
+  headline:
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, cupiditate?",
+  description:
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia libero, officiis eum dolore qui veritatis consectetur odio quibusdam error saepe, deleniti iure, velit corporis nesciunt.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: svg1,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
